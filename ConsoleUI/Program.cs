90c7c9d3fe8680b666acc9d5115f8db2bc4e049b
Zeynep Aydın <zeynoaydnn@gmail.com>
2023-06-29 15:18:59 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,50 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        //SOLID
+        //open closed principle
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //ProductTest();
+
+            //CategoryTest();
+
+            ProductTestDto();
+        }
+
+        private static void ProductTestDto()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var item in productManager.GetProductDetails())
+            {
+                Console.WriteLine(item.ProductName+" "+item.ProductId);
+            }
+        }
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var item in categoryManager.GetAll())
+            {
+                Console.WriteLine(item.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var item in productManager.GetByUnitPrice(20, 100))
+            {
+                Console.WriteLine(item.ProductName);
+            }
+        }
+
+
+    }
+}
